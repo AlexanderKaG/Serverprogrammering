8ec792fc08f1f49f8e6404e8a1730a99eb2e70cc
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-03-02 18:40:04 +0100

    Configured Student.java for Hibernate by adding @Entity, empty constructor and @Id/int Id variable

--- Workshops/HibernateProject/src/se/yrgo/domain/Student.java
+++ Workshops/HibernateProject/src/se/yrgo/domain/Student.java
@@ -1,24 +1,31 @@
 package se.yrgo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-public class Student
-{
-    private String enrollmentID;
-    private String name;
-    private String tutorName; // This will become a class soon
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) // This line is optional
+	private int Id;
+	private String enrollmentID;
+	private String name;
+	private String tutorName; // This will become a class soon
 
+	public Student() {
 
-    public Student(String name, String tutorName)
-    {
-    	this.name = name;
-    	this.tutorName = tutorName;
-    }
+	}
 
+	public Student(String name, String tutorName) {
+		this.name = name;
+		this.tutorName = tutorName;
+	}
 
-    public Student(String name)
-    {
-    	this.name = name;
-    	this.tutorName = null;
-    }
+	public Student(String name) {
+		this.name = name;
+		this.tutorName = null;
+	}
 
 }
