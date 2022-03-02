d1d53e5a46bd041005285eab2ca4fad97309a69f
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-03-02 18:55:14 +0100

    Configured HibernateTest.java to save data by adding factory, session and transaction

--- Workshops/HibernateProject/src/se/yrgo/test/HibernateTest.java
+++ Workshops/HibernateProject/src/se/yrgo/test/HibernateTest.java
@@ -1,14 +1,43 @@
 package se.yrgo.test;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
 import se.yrgo.domain.Student;
 
 public class HibernateTest {
 
+	private static SessionFactory sessionFactory = null;
+	
 	public static void main(String[] args) {
 		
+		SessionFactory sf = getSessionFactory();
+		Session session = sf.openSession();
+		
 		Student newStudent= new Student("Nick Fame", "Diamond Cameron");
-		System.out.println(newStudent);
+		session.save(newStudent); 
+		
+		Transaction tx = session.beginTransaction();
+		
+		tx.commit();
+		session.close();
 		
 	}
 
+	private static SessionFactory getSessionFactory() {
+		   if(sessionFactory ==null) {
+			Configuration configuration = new Configuration();
+			configuration.configure();
+			ServiceRegistry serviceRegistry = new ServiceRegistryBuilder()
+				.applySettings(configuration.getProperties())
+				.buildServiceRegistry();
+			sessionFactory = 
+		             configuration.buildSessionFactory(serviceRegistry);
+		   }
+		   return sessionFactory;
+		}
 }
