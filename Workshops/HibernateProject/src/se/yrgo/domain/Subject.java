8b5d186949c62e60bda89635999117490dc33cfd
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-03-03 18:41:35 +0100

    Added Subject.java according to workshop instructions and configured it for Hibernate

---
+++ Workshops/HibernateProject/src/se/yrgo/domain/Subject.java
@@ -0,0 +1,51 @@
+package se.yrgo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Subject {
+	
+	private int Id;
+	private String subjectName;
+	private int numberOfSemesters;
+
+	public Subject() {
+		
+	}
+	
+	public Subject(String subjectName, int numberOfSemesters) {
+		this.subjectName = subjectName;
+		this.numberOfSemesters = numberOfSemesters;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO) // This line is optional
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+
+	public int getNumberOfSemesters() {
+		return numberOfSemesters;
+	}
+
+	public void setNumberOfSemesters(int numberOfSemesters) {
+		this.numberOfSemesters = numberOfSemesters;
+	}
+	
+	
+}
