7e83d6630ea1e8116c798818296e8d72c5991e58
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-03-22 18:06:47 +0100

    Copy pasted code from workshop into HibernateTest.java

--- Workshops/HibernateProject/src/se/yrgo/test/HibernateTest.java
+++ Workshops/HibernateProject/src/se/yrgo/test/HibernateTest.java
@@ -6,6 +6,9 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import se.yrgo.domain.Student;
+import se.yrgo.domain.Tutor;
+
 public class HibernateTest {
 	public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("databaseConfig");
 
@@ -14,7 +17,11 @@ public class HibernateTest {
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 		
-		
+		Student newStudent = new Student("Eva Svensson","1-SVE-2019", "4th Street","Berlin","1010");
+		Tutor newTutor = new Tutor("3333", "Sara Reeves", 30000);
+				
+		em.persist(newStudent);
+		em.persist(newTutor);
 		
 		tx.commit();
 		em.close();
